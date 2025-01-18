a164b87cb329d9004f849276bf2d0b9c524de9ed
Asgard Juarez Campos <asgardprogramacion@gmail.com>
2025-01-17 20:18:27 -0600

    Agregar archivos de proyecto.

---
+++ ToDoListCLI/Class/interfaceClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListCLI.Class
+{
+    public class interfaceClass
+    {
+        public void RunView()
+        {
+            ShowMenu();
+        }
+        private void ShowMenu()
+        {
+            Console.WriteLine("Elije una opcion");
+            Console.WriteLine("1.- Mostrar tareas");
+            Console.WriteLine("2.- Crear tarea");
+            Console.WriteLine("3.- Modificar una tarea");
+            Console.WriteLine("4.- Cambiar estado de una tarea");
+            Console.WriteLine("5.- Cambiar prioridad de una tarea");
+            Console.WriteLine("6.- Salir");
+        }
+    }
+}
---
+++ ToDoListCLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ToDoListCLI.Class;
+namespace ToDoListCLI
+{
+    class ToDoListCLI
+    {
+        static void Main(string[] args)
+        {
+            var interFace = new interfaceClass();
+            interFace.RunView();
+        }
+    }
+}
